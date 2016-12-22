461bf97be48301dd1fd31d21a90002df4a7794dc
Pralave <pralave.94@outlook.com>
2016-12-22 15:56:08 +0530

    Face recognition finished

--- faceRecog.py
+++ faceRecog.py
@@ -30,5 +30,22 @@ def get_images_and_labels(path):
 	return images, labels
 
 path = "yalefaces"
-images. labels = get_images_and_labels(path)
+images, labels = get_images_and_labels(path)
 cv2.destroyAllWindows()
+recognizer.train(images, np.array(labels))
+
+image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.sad')]
+for image_path in image_paths:
+	predict_image_pil = Image.open(image_path).convert('L')
+	predict_image = np.array(predict_image_pil, "uint8")
+	faces = faceCascade.detectMultiScale(predict_image)
+	for (x, y, w, h) in faces:
+		nbr_predicted = recognizer.predict(predict_image[y: y+h, x: x+w])
+		nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+		if nbr_actual == nbr_predicted:
+			print "{} is Correctly Recognized".format(nbr_actual)
+		else:
+			print "{} is Incorrectly Recognized as {}".format(nbr_actual, nbr_predicted)
+		cv2.imshow("Face", predict_image[y: y+h, x: x+w],)
+		cv2.waitKey(1000)
+
